4945f7afe4afe03140d9bec6f81e859cd10c9205
Vitor-Silva27 <developer.vitorsilva@gmail.com>
2024-01-03 19:23:14 -0300

    fix icon sizes

--- src/components/Socials/components/SocialsItem.tsx
+++ src/components/Socials/components/SocialsItem.tsx
@@ -7,6 +7,6 @@ interface SocialsItemProps {
 }
 export const SocialsItem = ({icon, link, name}: SocialsItemProps) => {
   return (
-    <li><a href={link} target="_blank" rel="noreferrer"><Image className="transition-all hover:-translate-y-1 hover:brightness-0 object-contain" src={icon} alt={name} width={32} height={32} /></a></li>
+    <li><a href={link} target="_blank" rel="noreferrer"><Image className="transition-all h-8 hover:-translate-y-1 hover:brightness-0 object-contain" src={icon} alt={name} width={32} height={32} /></a></li>
   )
 }
