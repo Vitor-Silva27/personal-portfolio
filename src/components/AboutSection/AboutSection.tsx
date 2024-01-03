53e10ac01fcc68b4a890ae1d352de9fd82133ea5
Vitor-Silva27 <developer.vitorsilva@gmail.com>
2024-01-03 19:05:21 -0300

    fix eslint errors

--- src/components/AboutSection/AboutSection.tsx
+++ src/components/AboutSection/AboutSection.tsx
@@ -5,8 +5,8 @@ export const AboutSection = () => {
         <div className='w-full flex flex-col items-center justify-center lg:items-start sm:text-center lg:pl-16 lg:text-left xl:w-3/5'>
             <h2 className='font-display text-3xl sm:text-6xl tracking-widest mb-8' >A little bit about me</h2>
             <div className='w-10/12 2xl:w-4/6'>
-                <p className='text-zinc-600 text-base sm:text-xl my-2'>My name is Vitor Silva. I'm a programmer based in Brazil. I've been studying programming since 2020, and I'm passionate about development. I always strive to give my best in any work I do. </p>
-                <p className='text-zinc-600 text-base sm:text-xl my-2'>I am a quick learner, highly curious, and dedicated. My goal is to develop software solutions that improve and simplify people's lives, and have a great impact in society.</p>
+                <p className='text-zinc-600 text-base sm:text-xl my-2'>My name is Vitor Silva. I am a programmer based in Brazil. I have been studying programming since 2020, and I am passionate about development. I always strive to give my best in any work I do. </p>
+                <p className='text-zinc-600 text-base sm:text-xl my-2'>I am a quick learner, highly curious, and dedicated. My goal is to develop software solutions that improve and simplify people&apos;s lives, and have a great impact in society.</p>
             </div>
         </div>
     </section>
