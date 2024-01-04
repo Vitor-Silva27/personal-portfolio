d01031b7805a6a3f24c8b000faff0b1b07d8a4b1
Vitor-Silva27 <developer.vitorsilva@gmail.com>
2024-01-04 16:59:23 -0300

    fix overflow from about page

--- src/components/AboutSection/AboutSection.tsx
+++ src/components/AboutSection/AboutSection.tsx
@@ -1,6 +1,6 @@
 export const AboutSection = () => {
   return (
-    <section className="relative py-32 sm:py-64 flex w-full flex-col items-center justify-center lg:items-start lg:pl-16" id="about">
+    <section className="relative py-32 sm:py-64 flex w-full flex-col items-center justify-center lg:items-start lg:pl-16 overflow-x-hidden" id="about">
             <h2 className='font-display text-9xl md:text-10xl absolute text-slate-200 left-8 -z-20 tracking-widest mb-8 text-nowrap' >about me</h2>
             <div className='w-4/5 leading-relaxed'>
                 <p className='text-zinc-600 text-xl sm:text-4xl my-8 lg:ml-80'>My name is Vitor Silva. I am a programmer based in Brazil. I have been studying programming since 2020, and I am passionate about development. I always strive to give my best in any work I do. </p>
