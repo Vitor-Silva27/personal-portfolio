1a7d574276697670b8e2058d4d6213cf07b285d3
Vitor-Silva27 <developer.vitorsilva@gmail.com>
2024-01-04 18:25:14 -0300

    fix back to top button position

--- src/components/BackToTop/BackToTop.tsx
+++ src/components/BackToTop/BackToTop.tsx
@@ -35,23 +35,11 @@ export const BackToTop = () => {
         {showTopBtn && 
         <motion.div 
             onClick={goToTop} 
-            className="flex items-center justify-center w-16 h-16 rounded-full bg-zinc-950 fixed bottom-28 right-28 cursor-pointer z-50"
+            className="flex items-center justify-center w-16 h-16 rounded-full bg-zinc-950 fixed inset-x-0 mx-auto bottom-14 md:bottom-28 md:inset-x-auto  md:right-28 cursor-pointer z-50"
             initial={{ opacity: 0, y: 80, }}
             animate={{ opacity: 1, y: 0 }}
             exit={{ opacity: 0, y: 80 }}
-            transition={{
-                y: {
-                    duration: 0.5,
-                    ease: 'linear',
-                    repeat: 0,
-                },
-                opacity: {
-                    duration: 0.2,
-                    ease: 'easeInOut',
-
-                }
-            }
-            }
+            transition={{ duration: 0.3, ease: "linear" }}
         >
             <Image src={"/arrow-up.svg"} alt="arrow-up" width={22} height={12}/>
         </motion.div>}
