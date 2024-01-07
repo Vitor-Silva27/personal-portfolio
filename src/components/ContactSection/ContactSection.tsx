bdc421d5e2b87937cf087e40c740529b78562513
Vitor-Silva27 <developer.vitorsilva@gmail.com>
2024-01-07 11:20:27 -0300

    make contact form responsive

--- src/components/ContactSection/ContactSection.tsx
+++ src/components/ContactSection/ContactSection.tsx
@@ -30,16 +30,16 @@ export const ContactSection = () => {
     ]
 
   return (
-      <section id="contact" className="flex flex-col items-center font-display relative py-32 sm:py-96 z-10 bg-slate-100">
+      <section id="contact" className="flex flex-col sm:items-center font-display px-8 relative py-32 sm:py-96 z-10 bg-slate-100 mb-28">
         <h2 className="text-9xl md:text-10xl absolute text-slate-200 top-0 -z-20">Contact</h2>
         <div className="flex flex-col items-start justify-center">
-            <p className="font-body mb-2 text-xl text-zinc-700">Did you Like my portfolio?</p>
-            <h3 className="font-display text-9xl text-zinc-950">Get in touch!</h3>
+            <p className="font-body mb-2 sm:text-xl text-zinc-700">Did you Like my portfolio?</p>
+            <h3 className="font-display text-7xl sm:text-9xl text-zinc-950">Get in touch!</h3>
         </div>
-        <form className="flex flex-col gap-4 mt-10 w-3/4 max-w-xl font-body mb-8"  onSubmit={handleSubmit(onSubmit)}>
-            <div className="flex gap-4 mb-6">
-                <input  {...register('name', { required: true })} type="text" placeholder="Name" className="outline-none bg-transparent border-b-2 border-zinc-950 w-full text-xl focus:border-yellow-400 placeholder:text-zinc-700" />
-                <input  {...register('email', { required: true })} type="email" placeholder="Email" className="outline-none bg-transparent border-b-2 border-zinc-950 w-full text-xl focus:border-yellow-400 placeholder:text-zinc-700"/>
+        <form className="flex flex-col gap-4 mt-10 w-full sm:w-3/4 max-w-xl font-body mb-8"  onSubmit={handleSubmit(onSubmit)}>
+            <div className="flex flex-col sm:flex-row gap-4 mb-6">
+                <input  {...register('name', { required: true })} type="text" placeholder="Name" className="outline-none bg-transparent border-b-2 border-zinc-950 w-full text-xl focus:border-yellow-400 placeholder:text-zinc-700 py-6" />
+                <input  {...register('email', { required: true })} type="email" placeholder="Email" className="outline-none bg-transparent border-b-2 border-zinc-950 w-full text-xl focus:border-yellow-400 placeholder:text-zinc-700  py-6"/>
             </div>
             <textarea {...register('message', { required: true })} placeholder="Message" className="resize-none outline-none bg-transparent border-b-2 border-zinc-950 w-full text-xl focus:border-yellow-400 placeholder:text-zinc-700"/>
             <button type="submit" className="mt-16 flex justify-center items-center gap-12 p-4 h-20 hover:bg-yellow-400 hover:font-bold bg-zinc-950 text-zinc-100 hover:text-zinc-950">Send</button>
